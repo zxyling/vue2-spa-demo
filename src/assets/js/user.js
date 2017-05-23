7200fbf4b95ee4df6b7f6879c40d422d0338054a
zhangxiaoyang <zxyling@qq.com>
2017-05-23 09:46:23 +0800

    Update user.js

--- src/assets/js/user.js
+++ src/assets/js/user.js
@@ -1,10 +1,8 @@
 /**
  * @file 用户登录统一处理
- * @author dongkunshan
+ * @author 
  */
 
-// const loginUrl = 'http://wap.hwcareer.com/login';
-// const loginUrl = 'http://192.168.2.20:8090/login';
 const loginUrl = '/login';
 
 const login = (flag, url = window.location.href) => {
